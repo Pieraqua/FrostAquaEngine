32b81a7fcacfcf3e6b2be6240c52c1f0a2c48900
pieraqua <pauloffxii@hotmail.com>
2022-10-18 07:22:42 -0300

    flyweight map tiles

--- repos/RTSEngine_FrostAqua/RTSEngine_FrostAqua/Map.cpp
+++ repos/RTSEngine_FrostAqua/RTSEngine_FrostAqua/Map.cpp
@@ -7,19 +7,36 @@
 
 Map::Map(int x, int y)
 {
-	this->grid = new Tile* [x];
-	for (int i = 0; i < x; i++)
+	this->grid = new Tile* [x*y];
+	this->x_ = x; this->y_ = y;
+
+	/* Inicializa todos os tiles como planicie */
+	for (int i = 0; i < x * y; i++)
 	{
-		this->grid[i] = new Tile[y];
+		this->grid[i] = &plainsTile_;
 	}
-	this->x = x; this->y = y;
 }
 
 Map::~Map()
 {
-	for (int i = 0; i < this->x; i++)
-	{
-		delete this->grid[i];
-	}
 	delete this->grid;
+}
+
+bool Map::setTile(int x, int y, Tile* tile)
+{
+	if (x < this->x_ && y < this->y_)
+	{
+		this->grid[x * this->y_ + y] = tile;
+		return true;
+	}
+	return false;
+}
+
+Tile* Map::getTile(int x, int y)
+{
+	if (x < this->x_ && y < this->y_)
+	{
+		return this->grid[x * this->y_ + y];
+	}
+	return nullptr;
 }
--- repos/RTSEngine_FrostAqua/RTSEngine_FrostAqua/Map.h
+++ repos/RTSEngine_FrostAqua/RTSEngine_FrostAqua/Map.h
@@ -10,10 +10,20 @@ class Map{
 public:
 	Map(int x, int y);
 	~Map();
+	bool setTile(int x, int y, Tile* tile);
+	Tile* getTile(int x, int y);
 
 private:
-	int x = 0;
-	int y = 0;
+	int x_ = 0;
+	int y_ = 0;
+
+	/* Flyweight */
+	/* Caso sejam implementados tiles dinamicos, podemos fazer uma lista deles. */
 	Tile** grid = nullptr;
+	Tile plainsTile_ = Tile(Tile::Type::PLAINS);
+	Tile wallTile_ = Tile(Tile::Type::WALL);
+	Tile moundTile_ = Tile(Tile::Type::MOUND);
+	Tile rampTile_ = Tile(Tile::Type::RAMP);
+
 };
 
